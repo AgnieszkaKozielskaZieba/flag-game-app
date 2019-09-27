112b4493237364b9b3c56fb641b792180380cd7a
Agnieszka <agnieszka.kozielska@op.pl>
2019-09-27 15:03:02 +0200

    modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Game.css 	new file:   src/Game.js 	new file:   src/GuessForm.js

---
+++ src/GuessForm.js
@@ -0,0 +1,38 @@
+import React,{Component} from 'react';
+
+class GuessForm extends Component{
+	static defaultProps={
+		countriesToGuess:[],
+		handleSubmit(e){}
+	}
+	
+render(){
+	var selectedCountry=""
+	var {countriesToGuess}=this.props
+	let countriesInput=countriesToGuess.map((c,i)=>{
+  		return(
+  			<div key={i}>
+  			<input type="radio" id={c.name} name="countriesInput" value={c.name}
+  			onChange={(e)=>
+  				selectedCountry=e.target.value
+  			}/>
+  			<label htmlFor={c.name}>{c.name}</label>
+  			</div>
+  			)
+  	})
+	return(
+		<form className="GuessForm"
+		onSubmit={e=>{
+			e.preventDefault()
+			this.props.handleSubmit(selectedCountry)
+		}}
+		>
+	  		{countriesInput}
+	  		<button type="submit">
+	  		GUESS
+	  		</button>
+  		</form>
+		)
+}
+}
+export default GuessForm
